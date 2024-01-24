539839a164760e9cccb791fb07a362a4800a41c8
Matheus Gabriel <matheusgabriel.d.barros@gmail.com>
2024-01-24 15:33:35 -0300

    finish

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,16 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "photo":"link",
+        "title":"Titulo da noticia",
+        "description":"descrição da noticia",
+        "photoCover":"https://img.quizur.com/f/img5f0c80e0bd9d08.31973740.jpg?lastEdited=1594654954",
+    },
+    {
+        "id":"2",
+        "photo":"link",
+        "title":"Titulo da noticia",
+        "description":"descrição da noticia",
+        "photoCover":"",
+    }
+]
